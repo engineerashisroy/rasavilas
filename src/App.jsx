a79345183e29f1946999dafbfcbd7e6f44c2da8f
Ashis-Roy <as.ashisroy@gmail.com>
2023-12-08 06:46:40 +0530

    final comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
